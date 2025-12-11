7511e0598502c1b14565471cad152a1a16f1c36d
Kyrr Josh C. Magpusao <joshkyrr@gmail.com>
2025-12-11 21:51:59 +0800

    Add functions to replace words and count characters

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function replaceWord() {
+    const sentence = document.getElementById("sentence").value;
+    const target = document.getElementById("target").value;
+    const replacement = document.getElementById("replacement").value;
+    
+    const result = sentence.replace(target, replacement);
+    document.getElementById("replaceResult").textContent = result;
+}
 
+
+function countChars() {
+    const text = document.getElementById("charInput").value.trim();
+    document.getElementById("charResult").textContent = text.length;
+
+}
